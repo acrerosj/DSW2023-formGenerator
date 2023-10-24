2e08f166d6e9e40d60cec39f5700c052ab6bcfff
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-10-24 18:55:04 +0100

    add class SimpleField

--- Field.php
+++ Field.php
@@ -1,17 +1,18 @@
 <?php
 
-class Field{
-    private $name;
-    private $type;
+abstract class Field{
+    protected $name;
+    protected $type;
+    protected $text;
+    protected $default;
 
-    public function __construct($name, $type)
+    public function __construct($name, $type, $text, $default='')
     {
         $this->name = $name;
         $this->type = $type;
+        $this->text = $text;
+        $this->default = $default;
     }
     
-    public function render() {
-        echo "<p><label for=\"id_$this->name\">$this->name: </label>\n";
-        echo "<input id=\"id_$this->name\" type=\"$this->type\" name=\"$this->name\"></input></p>\n";
-    }
+    abstract public function render();
 }
